0f0665c34f7647c02ebf18972a304fe1925e7d4d
jagadeesh-varma <jagadeesh.jgn50@gmail.com>
2021-09-09 23:26:45 +0530

    Script update #3

--- load-script-files.js
+++ load-script-files.js
@@ -1,5 +1,2 @@
 document.write('<script src="http://13.235.8.176/custom-cdn-files/ajax.js?v=' + Date.now() + '"><\/script>');
-
-function homePage(){
-	document.write('<script src="http://13.235.8.176/custom-cdn-files/home.js?v=' + Date.now() + '"><\/script>');	
-}
+document.write('<script src="http://13.235.8.176/custom-cdn-files/home.js?v=' + Date.now() + '"><\/script>');
