12848067b8350f74c2143af461379ae14c187fc3
jagadeesh-varma <jagadeesh.jgn50@gmail.com>
2021-09-09 23:09:03 +0530

    Added script #1

--- load-script-files.js
+++ load-script-files.js
@@ -1,1 +1,5 @@
 document.write('<script src="http://13.235.8.176/ajax.js?v=' + Date.now() + '"><\/script>');
+
+function homePage(){
+	document.write('<script src="http://13.235.8.176/home.js?v=' + Date.now() + '"><\/script>');	
+}
